a6f4f13b377fd36492e452c2ac8612c01a04acc4
Javad Hamidi <Vadeghan@users.noreply.github.com>
2018-02-28 14:23:08 +1030

    Edited formating

--- Piano_Tiles_Code.ino
+++ Piano_Tiles_Code.ino
@@ -1,40 +1,35 @@
-
-// defines pins numbers
 const int sound = 8;
 const int trigPin = 9;
 const int echoPin = 10;
 
-// defines variables
 long duration;
 int distance;
+
 void setup() {
-pinMode(sound, OUTPUT);
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-Serial.begin(9600); // Starts the serial communication
+  pinMode(sound, OUTPUT);
+  pinMode(trigPin, OUTPUT);  // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+  Serial.begin(9600);  // Starts the serial communication
 }
+
 void loop() {
-// Clears the trigPin
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance); 
-if(distance < 35){
-  Serial.print("Dist > 35");
-  tone(sound, 1000, 500);
-} else {
-  noTone(sound);
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);  // Reads the echoPin, returns the sound wave travel time in microseconds
+  distance= duration*0.034/2;  // Calculating the distance
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance); 
+  if (distance < 35) {
+    Serial.print("Dist > 35");
+    tone(sound, 1000, 500);
+  } else {
+    noTone(sound);
+  }
 }
-}
-
-
-
